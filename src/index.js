905f90758d654f2734155cb43884421bcddac4a2
ajakcyer <ajakcyer97@gmail.com>
2020-10-30 12:07:55 -0400

    done

--- src/index.js
+++ src/index.js
@@ -13,32 +13,104 @@ document.addEventListener("DOMContentLoaded", () => {
     }
   });
 
-  ////make fetch GET request to get all his toys
+
+  //// START: make fetch GET request to get all his toys
   fetch('http://localhost:3000/toys')
   .then(response => response.json())
   .then(data =>{
     renderAllToys(data)
   })
+  /////// END
 
-  ///Event listener for form submit preventing default
+
+
+  /// START: Event listener for form submit preventing default
   const newToyForm = document.querySelector('.add-toy-form')
   newToyForm.addEventListener('submit', event=>{
     event.preventDefault()
     const form = event.target
 
     ///need toy object
+    const newToy = {
+      'name': form.name.value,
+      'image': form.image.value,
+      'likes': 0
+    }
+
+    //toyObjConfig to add to fetch request
+    const toyObjConfig = {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+        'Accept': 'application/json'
+      },
+      body: JSON.stringify(newToy)
+    }
     
-    ////make fetch POST request to add toys from form
-    console.log(form.name.value)
-    console.log(form.image.value)
+    ////make fetch POST request to add toys from form with toyObjConfig
+    fetch("http://localhost:3000/toys", toyObjConfig)
+    .then(response => response.json())
+    .then(data =>{
+      renderToy(data)
+    })
+    .catch(error =>{
+      console.log("error:", error)
+    })
+
+    //reset form fields
+    form.reset()
+  })
+  ///// END
+
+
+  ///START: Increasing toy likes
+
+  //ike button event delegation needs closest relative #toy-collection div
+  const toyCollection = document.querySelector('#toy-collection')
+  toyCollection.addEventListener('click', event =>{
+    if (event.target.matches('.like-btn')){
+      //individual like btns:
+
+      /////UPDATE DATABASE BASED ON TEXT FROM SCREEN
+      const likeBtn = event.target
+      const toyCard = likeBtn.parentNode
+      const likeP = toyCard.querySelector('p')
+      const currentLikes = parseInt(likeP.textContent)
+
+      const toyObjConfig = {
+        method: 'PATCH',
+        headers: {
+          'Content-Type': 'application/json',
+          'Accept': 'application/json'
+        },
+        body: JSON.stringify({
+          'likes': currentLikes + 1
+        })
+      }
+      
+      /////fetch PATCH request
+      fetch(`http://localhost:3000/toys/${toyCard.dataset.id}`, toyObjConfig)
+      .then(response => response.json())
+      .then(object =>{
+        ///UPDATE DOM BASED ON NEW INFO FROM DATABASE
+        updateLikes(object)
+      })
+      .catch(console.log)
+
+
+      //////GET TOY DATASET ID FROM OBJECT RESPONSE
+      const updateLikes = (newToy) =>{
+        const toyId = document.querySelector(`[data-id="${newToy.id}"]`)
+        const toyLikes = toyId.querySelector('p')
+        toyLikes.innerHTML = `${newToy.likes} Likes`
+      }
+
+    }
   })
 
-
-
-
+  //// END
 
 });
-
 
 ////////HELPER FUNCTIONS///////
 
